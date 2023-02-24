3b6300b3ea424b9fbb5c2b5e65a59b2cbebffd41
nhatduc <ducdn21102001@gmail.com>
2023-02-24 16:42:14 +0700

    createRecycleView

--- app/src/main/java/com/example/project/activity/MainActivity.java
+++ app/src/main/java/com/example/project/activity/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.project.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 import com.example.project.R;
+import com.example.project.adapter.SettingAdapter;
+import com.example.project.model.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Setting> list = new ArrayList<>();
+        Setting c1 = new Setting("Đổi mật khẩu", R.drawable.ic_right);
+        Setting c2 = new Setting("Quên mật khẩu", R.drawable.ic_right);
+        Setting c3 = new Setting("Thay đổi số điện thoại", R.drawable.ic_right);
+        Setting c4 = new Setting("Điều khoản sử dụng",R.drawable.ic_right);
+        list.add(c1);
+        list.add(c2);
+        list.add(c3);
+        list.add(c4);
+
+        RecyclerView rec = findViewById(R.id.rec_chapters);
+        SettingAdapter adapter = new SettingAdapter(list);
+        rec.setLayoutManager(new LinearLayoutManager(this));
+        rec.setAdapter(adapter);
     }
 }
