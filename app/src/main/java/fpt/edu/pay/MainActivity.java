d2db74707b07c2b282ebbdf087e250dd41279414
Hà Gia Kính <giakinh2000@gmail.com>
2023-02-28 02:45:22 +0700

    navigation

--- app/src/main/java/fpt/edu/pay/MainActivity.java
+++ app/src/main/java/fpt/edu/pay/MainActivity.java
@@ -1,14 +1,34 @@
 package fpt.edu.pay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import fpt.edu.pay.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private NavHostFragment navHostFragment;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initNavigation();
+    }
+
+    private void initNavigation(){
+        navHostFragment = (NavHostFragment)
+                getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        navController =  navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController);
     }
 }
